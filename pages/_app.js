28a5819dd06d58d3e1cc05d37793ee054e7258b0
Киселев Алексей Борисович <a.b.kiselev@yandex.ru>
2022-09-07 10:24:07 +0300

    Добавил redux-persist

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,22 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout'
 import NextNProgress from "nextjs-progressbar";
-import { store } from '../redux/store'
+import store from '../redux/store'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+
+let persistor = persistStore(store);
 
 function MyApp({ Component, pageProps }) {
   return (   
     <>
       <Provider store={store}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <PersistGate loading={null} persistor={persistor}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </PersistGate>
       </Provider>
       <NextNProgress color="rgba(139, 123, 255, 1)" startPosition={0.7} options={{ showSpinner: false }}/>
     </> 
--- redux/store.js
+++ redux/store.js
@@ -3,13 +3,55 @@ import popupsReduser from "./slices/popupsSlice";
 import cartReduser from "./slices/cartSlice";
 import userReduser from "./slices/userSlice";
 
-export const store = configureStore({
-    reducer: {
-        popups: popupsReduser,
-        cart: cartReduser,
-        user: userReduser,
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import thunk from 'redux-thunk';
+
+// export const store = configureStore({
+//     reducer: {
+//         popups: popupsReduser,
+//         cart: cartReduser,
+//         user: userReduser,
+//       },
+// })
+
+
+const reducers = combineReducers({
+  popups: popupsReduser,
+  cart: cartReduser,
+  user: userReduser,
+});
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+  whitelist: ['cart', 'user'],
+  blacklist: ['popups'],
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
+    }),
 })
-  
+
 store.subscribe(() => console.log(store.getState()))
 
+export default store;
