a525bbf46b5c7aaf2ce4cfbca3518d5081d3415a
unknown <a.b.kiselev@yandex.ru>
2022-09-12 22:45:12 +0300

    Наполнен раздел сумок и лукбук

--- components/MainSlider.js
+++ components/MainSlider.js
@@ -9,7 +9,7 @@ function MainSlider({slidesPerView, auto}) {
       <section className={styles.slider}>
         <Slider slidesPerView={slidesPerView} auto={auto}>
           <div className={styles.slider__slide}>
-            <Image className={styles.slider__img} src="/images/test.jpg" layout='fill' objectFit='cover' alt="Логотип" />
+            <Image className={styles.slider__img} src="/images/main0.jpg" layout='fill' objectFit='cover' alt="Логотип" />
             <div className={styles.slider__headers}>
               <div>
                 <h2 className={styles.slider__title}>New collection 2022</h2>
@@ -21,13 +21,13 @@ function MainSlider({slidesPerView, auto}) {
           </div>
           
           <div className={styles.slider__slide}>
-            <Image className={styles.slider__img} src="/images/test.jpg" layout='fill' objectFit='cover' alt="Логотип" />
+            <Image className={styles.slider__img} src="/images/main2.jpg" layout='fill' objectFit='cover' alt="Логотип" />
             <div className={styles.slider__headers}>
               <div>
                 <h2 className={styles.slider__title}>New collection 2022</h2>
                 <p className={styles.slider__subtitle}>На сайте уже доступна новая коллекция!</p>
               </div>
-              <ButtonFilled text="Смотреть" url="/sumki" font="fz12" padd="p1235" />
+              <ButtonFilled text="Смотреть" url="/remni" font="fz12" padd="p1235" />
               
             </div>
           </div>
--- components/Zakaz.js
+++ components/Zakaz.js
@@ -24,9 +24,11 @@ function Zakaz() {
 
           <div className={styles.zakaz_descr}>
             <h3 className={styles.zakaz_title}>ИНДИВИДУАЛЬНЫЙ ПОШИВ</h3>
-            <p className={styles.zakaz_text}>Шью Шью Шью Шью Шью Шью Шью Шью Шью Шью Шью Шью Шью Шью Шью Шью Шью Шью Шью Шью Шью Шью Шью Шью Шью ШьюШью Шью Шью Шью Шью Шью Шью Шью Шью Шью Шью Шью ШьюШью Шью Шью Шью Шью Шью Шью Шью Шью Шью Шью Шью Шью</p>
-            <p className={styles.zakaz_text}>Шью Шью Шью Шью Шью ШьюШью Шью Шью Шью Шью Шью Шью Шью Шью Шью Шью Шью ШьюШью Шью Шью Шью Шью Шью Шью Шью Шью Шью Шью Шью Шью</p>
-            <ButtonArrow text="ОБСУДИТЬ ИДЕИ" url="#" font="fz12" onClick={handleClick}/>  
+            <p className={styles.zakaz_text}>Пошью ремень под цвет вашей сумочки. Подберем стропу из более 500 вариантов, фурнитуру из более 300 вариантов.</p>
+            <p className={styles.zakaz_text}>Текстильный ремень с кожаными вставками из итальянской кожи в наличии и под заказ.</p>
+            <p className={styles.zakaz_text}>Создадим красоту вместе!</p>
+            <p className={styles.zakaz_text}></p>
+            <ButtonArrow text="ОБСУДИТЬ ИДЕИ" url="#" font="fz14" onClick={handleClick}/>  
           </div>
         
       </div>
