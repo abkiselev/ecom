965bda116274bf593d6747bc1c12a98f6ed649ef
Киселев Алексей Борисович <a.b.kiselev@yandex.ru>
2022-09-13 12:42:12 +0300

    Откатился к версии до теста gh-pages

--- next.config.js
+++ next.config.js
@@ -6,7 +6,6 @@ const withTM = require('next-transpile-modules')(['@fancyapps/ui']);
 const nextConfig = {
   // reactStrictMode: true,
   swcMinify: true,
-  basePath: '/ecom',
 };
 
 module.exports = withTM(nextConfig);
